764dfd009ae78fe032394a0b5c5f2ed72b9f579f
Mark Timbol <Mark@Mark.local>
2016-04-26 12:18:05 +0400

    Update config

--- wp-config.php
+++ wp-config.php
@@ -21,7 +21,7 @@
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', getenv('DB_NAME'));
-echo getenv('DB_NAME');
+
 define('DB_USER', getenv('DB_USER'));
 
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
@@ -44,14 +44,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         '!8r,c4mvGW?,pX${lEwU![;O,t&n~+[+J3SCGc7|`D:g5`yt`Wlb{8t-HZVWLgVV');
+define('SECURE_AUTH_KEY',  '@_HqH0i9GI+nSkLs`^V6_V .r@C7VZf :q+FPqi(b2[UU<$|y=2T/F`@=B1#)h%[');
+define('LOGGED_IN_KEY',    'E}%WeRA<JVF32>EVEV7fiylV@#}!f:Ka]1S%aj>yd)KU20s.XV.p9$.nO#D*H&<)');
+define('NONCE_KEY',        '[1)|`0O>I-`Nz<=xRUsnIK~?B]l!&8{;y!o1Sv0~#eyB<w.+s{p<vNq3z:?>ziRV');
+define('AUTH_SALT',        'ki&_=)Gev:R3Pr9F8u(X(=TI@IyzSkDCxXa-:k<v4#0+l_:|-bCaoV^Jr Fv.-<e');
+define('SECURE_AUTH_SALT', 'lQ[|6V(nr=jv(n{7FAQ<>|Q%]l:&3csY^e]R5K->7+w;-A+?.X6k(0n;uWm<]d5L');
+define('LOGGED_IN_SALT',   '-BN0GO$BRVr<Y/XJN}JSt&S>hoa&$0f}f&tL<SR=|k|778BoLX4.]|#j2rMR+Kvm');
+define('NONCE_SALT',       '_o+M+%%`+(B;JI#Pl{RU @DZ8rzS_Z{ hok/tnfqQ6`]Jq| |=8y*)!9WbTj;zT!');
 
 /**#@-*/
 
