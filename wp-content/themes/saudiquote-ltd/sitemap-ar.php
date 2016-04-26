7a878f7bb29bb8d37cb192225dd0c96b0eab5d12
Mark Timbol <Mark@Mark.local>
2016-04-26 12:33:10 +0400

    Sitemap

---
+++ wp-content/themes/saudiquote-ltd/sitemap-ar.php
@@ -0,0 +1,99 @@
+		<div class="row risk-warning">
+			<div class="small-12 medium-12 large-12 columns">
+				<div class="">
+					<p class="">تلتزم <strong>“SaudiQuote”</strong> بتطبيق كافة القوانين والمبادئ الاسلامية في جميع استثماراتها وذلك من خلال الاشراف عليها من هيئات شرعية معتمدة كما نحرص على تقديم كافة النصائح والمشورات لجميع عملائنا.</p>
+					<p class="">نود أن نُحيطكم عِلمآ باننا لا نتعامل إلا بمصادر إسلامية.</p>
+				</div>
+
+				<div class="">
+					<p>تحذير المخاطر: تجارة المشتقات يحمل درجة عالية من المخاطرة برأس المال، ويجب أن تتاجر فقط مع المال الذي يمكن أن تحمل خسارته. قد لا تكون تجارة المشتقات مناسبة لجميع المستثمرين، لذلك يرجى التأكد من أنك تفهم تماما المخاطر التي تنطوي عليها، والحصول على مشورة مستقلة إذا لزم الأمر. يُرجى قراءة <a href="http://saudiquote.com/docs/Customer-Agreement-Arabic.pdf" target="_blank">الشروط والأحكام</a> قبل بدء التداول.</p>
+					<p>تعد <strong>“SaudiQuote”</strong> مؤسسة مسجلة كمزود للخدمات المالية في سجل مزودي الخدمات المالية نيو زيلندا  ” FSPR” تحت رقم : FSP329966، وعضواً في مؤسسة تسوية المنازعات المالية “FSCL” تحت رقم: 5099″.</p>
+					<p>في حال عدم رضاك عن جواب شكواك يمكنك تصعيد استفسارك الى FSCL بالضغط <a href="http://www.fscl.org.nz/index.html?Action=DirectoryItem&amp;ID=28635078" target="_blank">هنا</a>.</p>
+					<p>العنوان :الطابق ١، شارع ديكنس ١، نابير ٤١١٠، نيو زيلندا</p>
+				</div>
+
+			</div>
+		</div>
+
+		<footer>
+			<div class="sitemap">
+				<div class="row sitemap-list">
+					<?php wp_nav_menu( array('menu' => 'MainMenu' )); ?>
+					<?php /*
+					<div class="small-6 medium-2 large-2 columns">
+						<h6>نبذة عن الشركة</h6>
+						<ul>
+							<li><a href="<?=get_permalink(1894);?>">من نحن</a></li>
+            			</ul>
+					</div>
+
+					<div class="small-6 medium-2 large-2 columns">
+						<h6><a href="<?=get_permalink(1927);?>">الحزمات الاستثمارية</a></h6>
+						<ul>
+							<li><a href="<?=get_permalink(1929);?>">الفوركس</a></li>
+            			</ul>
+
+						<h6><a href="<?=get_permalink(1919);?>">المنصات</a></h6>
+						<ul>
+							<li><a href="<?=get_permalink(1925);?>">ميتاتريدر</a></li>
+							<li><a href="<?=get_permalink(1923);?>">جي . تي . اكس</a></li>
+            			</ul>            			
+					</div>
+
+					<div class="small-6 medium-2 large-2 columns">
+						<h6><a href="<?=get_permalink(1907);?>">لنبدأ</a></h6>
+						<ul>
+							<li><a href="<?=get_permalink(1938);?>">ما هو الفوركس؟</a></li>
+							<li><a href="<?=get_permalink(1909);?>">بالنسبة للمبتدئين</a></li>
+							<li><a href="<?=get_permalink(1917);?>">فتح حساب حقيقي</a></li>
+							<li><a href="<?=get_permalink(1915);?>">فتح حساب تجريبي</a></li>
+							<li><a href="<?=get_permalink(2306);?>">عملية فتح الحساب</a></li>
+							<li><a href="<?=get_permalink(1911);?>">الأسئلة الأكثر شيوعاً عن سوق الفوركس</a></li>
+							<li><a href="<?=get_permalink(1913);?>">الأسئلة الشائعة عن "SAUDIQUOTE"</a></li>
+            			</ul>
+					</div>
+
+					<div class="small-6 medium-2 large-2 columns">
+						<h6><a href="<?=get_permalink(1934);?>">التداول</a></h6>
+						<ul>
+							<li><a href="<?=get_permalink(1936);?>">أدوات التداول</a></li>
+							<li><a href="<?=get_permalink(1905);?>">المفكرة الاقتصادية</a></li>
+							<li><a href="">الملخص الأسبوعي</a></li>
+							<li><a href="<?=get_permalink(1900);?>">أخبار السوق</a></li>
+            			</ul>
+					</div>	
+
+					<div class="small-6 medium-2 large-2 columns">
+						<h6><a href="<?=get_permalink(2388);?>">للتحميل</a></h6>
+						<ul>
+							<li><a href="<?=get_permalink(2390);?>">الدروس</a></li>
+							<li><a href="<?=get_permalink(2386);?>">المنصات</a></li>
+							<li><a href="<?=get_permalink(2392);?>">القانوني</a></li>
+							<li><a href="<?=get_permalink(2621);?>">دليل التداول</a></li>
+            			</ul>
+					</div>	
+
+					<div class="small-6 medium-2 large-2 columns">
+						<h6><a href="<?=get_permalink(1898);?>">للتواصل</a></h6>
+					</div>					
+					*/ ?>
+					<div class="clearfix"></div>																										
+				</div>
+			</div>
+
+			<p class="text-center">
+				<a class="sitemap-link sitemap-close">خريطة الموقع</a>
+			</p>
+		
+			<div class="row">
+				<div class="small-3 medium-2 large-2 columns footer-logo">
+					<img src="<?=get_bloginfo('stylesheet_directory');?>/images/footer-logo.png" alt="SaudiQuote Logo" />
+				</div>
+				<div class="small-9 medium-10 large-10 columns">
+					<p class="copyright">
+						Saudiquote.com مملوكة وبإدارة شركة SaudiQuote المحدودة في نيوزيلندا (الهاتف : <span dir="ltr">+64 6 8243922</span>)<br />
+						جميع الحقوق محفوظة &copy; <?=date('Y');?> SaudiQuote المحدودة | <a href="#">تنصل</a> | <a href="#">سياسة الخصوصية</a> | <a href="http://saudiquote.com/docs/Customer-Agreement-Arabic.pdf" target="_blank">الشروط والأحكام</a>
+					</p>
+				</div>
+			</div>
+		</footer>
---
+++ wp-content/themes/saudiquote-ltd/sitemap-en.php
@@ -0,0 +1,101 @@
+			<div class="row risk-warning">
+				<div class="small-12 medium-12 large-12 columns">
+					<div class="">
+						<p class="">
+							<strong>“SaudiQuote”</strong> is committed to comply with all laws and Islamic principles in force in all its investments, through supervision by accredited Sharia bodies. Furthermore, we are keen on providing advice and recommendations to all our clients and improving by offering a relevant code of professional conduct.
+						</p>
+					</div>
+
+					<div class="">
+						<p><strong>Risk Warning</strong>: Trading Derivatives carries a high level of risk to your capital and you should only trade with money you can afford to lose. Trading Derivatives may not be suitable for all investors, so please ensure that you fully understand the risks involved, and seek independent advice if necessary. Please read our <a href="http://saudiquote.com/docs/Customer-Agreement-English.pdf" target="_blank">terms and conditions</a> before trading. </p>
+
+						<p>“SaudiQuote” Limited is registered with the Financial Service Providers Register in New Zealand (FSPR) with registration number is FSP329966 and is a member of the Financial Services Complaints Limited (FSCL) with member number 5099.</p>
+
+						<p>If you’re not satisfied with an answer to your complaint, you can escalate your inquiry to the FSCL by clicking <a href="http://www.fscl.org.nz/index.html?Action=DirectoryItem&amp;ID=28635078" target="_blank">here</a>.</p>
+
+						<p>Registered office: Level 1, 1 Dickens Street, Napier 4110, New Zealand</p>
+					</div>
+				</div>
+			</div>
+
+			<footer>
+				<div class="sitemap">
+					<div class="row sitemap-list">
+						<?php wp_nav_menu( array('menu' => 'MainMenu' )); ?>
+						<?php /*						
+						<div class="small-6 medium-2 large-2 columns">
+							<h6>Company Profile</h6>
+							<ul>
+								<li><a href="<?=get_permalink(1021);?>">About Us</a></li>
+								<li><a href="<?=get_permalink(1857);?>">Our Vision</a></li>
+								<li><a href="<?=get_permalink(1611);?>">Contact Us</a></li>
+	            			</ul>
+						</div>
+
+						<div class="small-6 medium-2 large-2 columns">
+							<h6><a href="<?=get_permalink(1547);?>">Products</a></h6>
+							<ul>
+								<li><a href="<?=get_permalink(1556);?>">Forex</a></li>
+	            			</ul>
+
+							<h6><a href="<?=get_permalink(1203);?>">Platforms</a></h6>
+							<ul>
+								<li><a href="<?=get_permalink(1205);?>">MetaTrader 4</a></li>
+								<li><a href="<?=get_permalink(1209);?>">GTX</a></li>
+	            			</ul>            			
+						</div>
+
+						<div class="small-6 medium-2 large-2 columns">
+							<h6><a href="<?=get_permalink(1063);?>">Get Started</a></h6>
+							<ul>
+								<li><a href="<?=get_permalink(1135);?>">What is Forex</a></li>
+								<li><a href="<?=get_permalink(1065);?>">For Beginners</a></li>
+								<li><a href="<?=get_permalink(1109);?>">Open a Live account</a></li>
+								<li><a href="<?=get_permalink(1107);?>">Open a Demo account</a></li>
+								<li><a href="<?=get_permalink(2293);?>">Account Opening</a></li>
+								<li><a href="<?=get_permalink(1069);?>">Forex market FAQ</a></li>
+								<li><a href="<?=get_permalink(1071);?>">SaudiQuote FAQ</a></li>
+	            			</ul>
+						</div>
+
+						<div class="small-6 medium-2 large-2 columns">
+							<h6><a href="<?=get_permalink(1073);?>">Trading</a></h6>
+							<ul>
+								<li><a href="<?=get_permalink(1077);?>">Trading Instruments</a></li>
+								<li><a href="<?=get_permalink(1132);?>">Economic Calendar</a></li>
+								<li><a href="#">Weekly Wrapup</a></li>
+								<li><a href="<?=get_permalink(1041);?>">Market News</a></li>
+	            			</ul>
+						</div>	
+
+						<div class="small-6 medium-2 large-2 columns">
+							<h6><a href="<?=get_permalink(2297);?>">Downloads</a></h6>
+							<ul>
+								<li><a href="<?=get_permalink(2303);?>">Tutorials</a></li>
+								<li><a href="<?=get_permalink(2301);?>">Platforms</a></li>
+								<li><a href="<?=get_permalink(2299);?>">Legal</a></li>
+								<li><a href="<?=get_permalink(2607);?>">Trading Handbook</a></li>
+	            			</ul>
+						</div>	
+						*/ ?>
+
+						<div class="clearfix"></div>																										
+					</div>
+				</div>
+
+				<p class="text-center">
+					<a class="sitemap-link sitemap-close">Sitemap</a>
+				</p>
+			
+				<div class="row">
+					<div class="small-3 medium-2 large-2 columns footer-logo">
+						<img src="<?=get_bloginfo('stylesheet_directory');?>/images/footer-logo.png" alt="SaudiQuote Logo" />
+					</div>
+					<div class="small-9 medium-10 large-10 columns">
+						<p class="copyright">
+							Saudiquote.com is owned and operated by SAUDIQUOTE LIMITED, New Zealand (Tel: + 64 6 8243922)<br />
+							Copyright &copy; <?=date('Y')?> SaudiQuote Ltd. | <a href="#">Disclaimer</a> | <a href="#">Privacy Policy</a> | <a href="http://saudiquote.com/docs/Customer-Agreement-English.pdf" target="_blank">Terms & Conditions</a>
+						</p>
+					</div>
+				</div>
+			</footer>
